b04dbfc536488e99808843bfaf0e39bca2099282
carolineclark <caroline@kano.me>
2014-04-11 11:15:56 +0100

    Changed mouse setting UI

--- kano_settings/set_mouse.py
+++ kano_settings/set_mouse.py
@@ -48,25 +48,47 @@ def activate(_win, box, update):
     slow_button = Gtk.RadioButton.new_with_label_from_widget(None, "Slow")
     slow_button.connect("toggled", on_button_toggled)
     slow_button.set_can_focus(True)
+    slow_button.get_style_context().add_class("bold_toggle")
+    slow_info = Gtk.Label("REQUIRES LESS MOVE PRECISION")
+    slow_info.get_style_context().add_class("normal_label")
+
+    slow_box = Gtk.Box()
+    slow_box.pack_start(slow_button, False, False, 0)
+    slow_box.pack_start(slow_info, False, False, 0)
 
     # Normal radio button
     normal_button = Gtk.RadioButton.new_from_widget(slow_button)
     normal_button.set_label("Normal")
     normal_button.connect("toggled", on_button_toggled)
     normal_button.set_can_focus(False)
+    normal_button.get_style_context().add_class("bold_toggle")
+    normal_info = Gtk.Label("THE DEFAULT SETTING")
+    normal_info.get_style_context().add_class("normal_label")
+
+    normal_box = Gtk.Box()
+    normal_box.pack_start(normal_button, False, False, 0)
+    normal_box.pack_start(normal_info, False, False, 0)
 
     # Fast radio button
     fast_button = Gtk.RadioButton.new_from_widget(slow_button)
     fast_button.set_label("Fast")
     fast_button.connect("toggled", on_button_toggled)
     fast_button.set_can_focus(False)
+    fast_button.get_style_context().add_class("bold_toggle")
+    fast_info = Gtk.Label("BETTER FOR WIDE SCREENS")
+    fast_info.get_style_context().add_class("normal_label")
+
+    fast_box = Gtk.Box()
+    fast_box.pack_start(fast_button, False, False, 0)
+    fast_box.pack_start(fast_info, False, False, 0)
 
     radio_button_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
-    radio_button_container.pack_start(slow_button, False, False, 10)
-    radio_button_container.pack_start(normal_button, False, False, 10)
-    radio_button_container.pack_start(fast_button, False, False, 10)
+    radio_button_container.pack_start(slow_box, False, False, 5)
+    radio_button_container.pack_start(normal_box, False, False, 5)
+    radio_button_container.pack_start(fast_box, False, False, 5)
 
-    valign = Gtk.Alignment(xalign=0.5, yalign=0, xscale=0, yscale=0)
+    valign = Gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0, yscale=0)
+    valign.set_padding(100, 0, 0, 0)
     valign.add(radio_button_container)
     settings.box.pack_start(valign, False, False, 0)
 
