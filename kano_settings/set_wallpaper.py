57645e4fcbcbeb6a80dd4e584ae40477d0522559
asimon <alejandro@kano.me>
2014-04-29 10:49:14 +0100

    Modified kano-settings with new kdeskrc behaviour https://github.com/KanoComputing/peldins/issues/743

--- kano_settings/set_wallpaper.py
+++ kano_settings/set_wallpaper.py
@@ -105,13 +105,44 @@ class Wallpaper():
         # home directory
         USER = os.environ['SUDO_USER']
         deskrc_path = kdeskrc_home % (USER)
+        # Wallpaper selected
+        image_169 = "%s%s-16-9.png" % (wallpaper_path, image_name)
+        image_43 = "%s%s-4-3.png" % (wallpaper_path, image_name)
+        image_1024 = "%s%s-1024.png" % (wallpaper_path, image_name)
+        # Look for the strings
+        found = False
+        if os.path.isfile(deskrc_path):
+            f = open(deskrc_path, 'r')
+            newlines = []
+            for line in f:
+                if "Background.File-medium:" in line:
+                    line = "  Background.File-medium: %s\n" % (image_1024)
+                    found = True
+                elif "Background.File-4-3:" in line:
+                    line = "  Background.File-4-3: %s\n" % (image_43)
+                elif "Background.File-16-9:" in line:
+                    line = "  Background.File-16-9: %s\n" % (image_169)
+                newlines.append(line)
+            f.close()
+        if found:
+            # Overwrite config file with new lines
+            outfile = open(deskrc_path, 'w')
+            outfile.writelines(newlines)
+            outfile.close()
+        # If not found add it
+        else:
+            with open(deskrc_path, "a") as outfile:
+                outfile.write("  Background.File-medium: %s\n" % (image_1024))
+                outfile.write("  Background.File-4-3: %s\n" % (image_43))
+                outfile.write("  Background.File-16-9: %s\n" % (image_169))
+        '''
         if not os.path.isfile(deskrc_path):
             try:
                 shutil.copyfile(kdeskrc_default, deskrc_path)
             except:
                 return 1
 
-         # Change wallpaper in deskrc
+        # Change wallpaper in deskrc
         image_169 = "%s%s-16-9.png" % (wallpaper_path, image_name)
         image_43 = "%s%s-4-3.png" % (wallpaper_path, image_name)
         image_1024 = "%s%s-1024.png" % (wallpaper_path, image_name)
@@ -132,7 +163,7 @@ class Wallpaper():
         outfile.writelines(newlines)
         outfile.flush()
         outfile.close()
-
+        '''
         # Refresh the wallpaper
         cmd = 'sudo -u %s kdesk -w' % USER
         os.system(cmd)
