4022216ab802ab67396a175342b7d19ba66448ce
Zsolt Ero <zsolt.ero@gmail.com>
2014-04-30 18:36:07 +0100

    read_file_contents_as_lines

--- kano_settings/screen/screen_config.py
+++ kano_settings/screen/screen_config.py
@@ -12,14 +12,14 @@ import os
 import sys
 import re
 import subprocess
+from kano.utils import read_file_contents_as_lines
 
 CONFIG_FILE = "/boot/config.txt"
 
 
 # if the value argument is None, the option will be commented out
 def set_config_option(name, value=None):
-    with open(CONFIG_FILE, "r") as config_file:
-        lines = config_file.readlines()
+    lines = read_file_contents_as_lines(CONFIG_FILE)
 
     option_re = r'^\s*#?\s*' + str(name) + r'=(.*)'
 
