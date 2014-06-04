ff48a8004092af349578bd699388422b064a0283
carolineclark <caroline@kano.me>
2014-06-04 18:28:41 +0100

    Changed styling of Do You Want To Reboot? screen

--- kano_settings/components/first_run.py
+++ kano_settings/components/first_run.py
@@ -121,7 +121,8 @@ def close_window(event="delete-event", button=None):
     if constants.need_reboot:
         kdialog = kano_dialog.KanoDialog("Reboot?",
                                          "Your Kano needs to reboot for changes to apply",
-                                         {"REBOOT NOW": {"return_value": 1}, "LATER": {"return_value": 0}})
+                                         {"REBOOT NOW": {"return_value": 1, "color": "orange"}, "LATER": {"return_value": 0, "color": "grey"}})
+        kdialog.set_action_background("grey")
         response = kdialog.run()
         if response == 1:
             os.system("sudo reboot")
