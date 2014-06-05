1536c7dc4914760a64cca88b90466fd5c18d2229
Albert Casals <skarbat@gmail.com>
2014-06-05 14:05:00 +0100

    Kano settings parental feature for Chromium and other Internet apps

--- kano_settings/set_advance/home.py
+++ kano_settings/set_advance/home.py
@@ -14,7 +14,7 @@ import kano_settings.components.fixed_size_box as fixed_size_box
 from ..config_file import get_setting, set_setting
 from kano.utils import run_cmd
 from kano.logging import set_system_log_level
-import crypt
+import os, crypt, shutil
 
 win = None
 update = None
@@ -130,10 +130,41 @@ def update_config():
         fake_password = 'Fake123Password'
         set_setting("Parental-password", fake_password)
         set_setting("Parental-lock", crypt.crypt ('True', fake_password))
+        set_hosts_blacklist (enable=True)
     else:
         set_setting("Parental-password", "")
         set_setting("Parental-lock", 'False')
+        set_hosts_blacklist (enable=False)
 
+
+def set_hosts_blacklist(enable=True, blacklist_file='/usr/share/kano-settings/media/Parental/parental-hosts-blacklist.gz'):
+    blacklisted = False
+    hosts_file = '/etc/hosts'
+    hosts_file_backup = '/etc/kano-hosts-parental-backup'
+    bare_hosts = [ '127.0.0.1 kano', '127.0.0.1 localhost' ]
+
+    if enable:
+        # Populate a list of hosts which should not be reached (Parental browser protection)
+        if os.stat(hosts_file).st_size > 1024*10:
+            # sanity check: this is a big file, looks like the blacklist is already in place!
+            pass
+        else:
+            # make a copy of hosts file and APPEND it with a list of blacklisted internet hostnames
+            shutil.copyfile (hosts_file, hosts_file_backup)
+            rc = os.system ('zcat %s >> %s' % (blacklist_file, hosts_file))
+            blacklisted = True
+    else:
+        # Restore the original list of hosts
+        try:
+            os.stat (hosts_file_backup)
+            shutil.copy (hosts_file_backup, hosts_file)
+        except:
+            # the backup is gone, recreate it simply by the bare minimum needed
+            with open (hosts_file, 'wt') as hhh:
+                for host in bare_hosts:
+                    hhh.write (host + '\n\r')
+
+    return blacklisted
 
 # Returns True if all the entries are the same as the ones stored in the config file.
 def compare():
