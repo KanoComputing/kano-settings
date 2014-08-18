0707b8256d9dc1ab3fcd06651ff3b9b198277952
Zsolt Ero <zsolt.ero@gmail.com>
2014-08-18 16:14:54 +0100

    cleaned like crazy

--- kano_settings/system/proxy.py
+++ kano_settings/system/proxy.py
@@ -7,9 +7,7 @@
 #
 
 
-from kano.utils import read_file_contents_as_lines
-from kano.logging import logger
-from kano.utils import get_user_unsudoed, run_bg, run_cmd
+from kano.utils import read_file_contents_as_lines, write_file_contents, run_cmd
 
 chromium_cfg = '/etc/chromium/default'
 dante_conf = '/etc/dante.conf'
@@ -20,7 +18,7 @@ template_dante_socks = """
 # kano-settings generated dante.conf
 
 route {
-   from: 0.0.0.0/0   to: 0.0.0.0/0   via: %(proxy-ip)s port = %(proxy-port)s
+   from: 0.0.0.0/0   to: 0.0.0.0/0   via: {proxyip} port = {proxyport}
    protocol: tcp udp
    proxyprotocol: socks_v4 socks_v5
 }
@@ -30,14 +28,14 @@ template_dante_http = """
 # kano-settings generated dante.conf
 
 route {
-   from: 0.0.0.0/0   to: 0.0.0.0/0   via: %(proxy-ip)s port = %(proxy-port)s
+   from: 0.0.0.0/0   to: 0.0.0.0/0   via: {proxyip} port = {proxyport}
    command: connect
    proxyprotocol: http_v1.0
 }
 """
 
 
-def set_chromium_proxy(ip, port, ptype, username, password, enable=True):
+def set_chromium(enable, ip, port, ptype, username, password):
     if enable:
         if 'socks_v5' in ptype:
             proxy_type = 'socks5'
@@ -49,160 +47,64 @@ def set_chromium_proxy(ip, port, ptype, username, password, enable=True):
         strflags = '"--password-store=detect"'
 
     cmd = "/bin/sed -i 's/CHROMIUM_FLAGS=.*/CHROMIUM_FLAGS=%s/g' %s" % (strflags, chromium_cfg)
-    _, _, rc = run_cmd(cmd)
-    return rc == 0
+    run_cmd(cmd)
+    return
 
 
-def get_chromium_proxy():
-    lines = read_file_contents_as_lines(chromium_cfg)
-    for line in lines:
-        if '--proxy-server' in line:
-            return True
-
-
-def lib_preload_is_enabled():
-    enabled_modules = read_file_contents_as_lines(ld_so_preload_file)
-    for module in enabled_modules:
-        if module in ld_so_preload_libs:
-            return True
-    return False
-
-
-class LibPreload:
-
-    def proxify(self, enable=False):
-
-        # Chromium settings go to its config file. Change them now
-        p = ProxySettings().get_settings()
-        if not p:
-            logger.error('error setting chromium proxy')
-            return
-
-        logger.info('setting chromium')
-        set_chromium_proxy(p['proxy-ip'], p['proxy-port'], p['proxy-type'], None, None, enable)
-
-        # If the change is already set, do nothing
-        if enable == self.is_enabled():
-            return True
-
-        # Collect currently preloaded libraries
-        entries = read_file_contents_as_lines(self.filename)
-
-        # Add or remove proxy libraries
-        if enable:
-            for lib in self.libs:
-                entries.append(lib)
+def set_dante(enable, proxyip, proxyport, proxytype, username=None, password=None):
+    if enable:
+        if proxytype == 'http_v1.0':
+            new_settings = template_dante_http.format(proxyip=proxyip, proxyport=proxyport)
         else:
-            for l in self.libs:
-                entries.remove(l + '\n')
+            new_settings = template_dante_socks.format(proxyip=proxyip, proxyport=proxyport)
+    else:
+        new_settings = ''
 
-        # Write back the prelaod file
-        f = open(self.filename, 'w')
-        for e in entries:
-            if (e != '\n'):
-                f.write("%s\n" % e)
-        f.close()
-        return True
+    write_file_contents(dante_conf, new_settings)
+    update_ld_so_preload(enable)
 
 
-
-
-
-def parse_out(rs):
-    settings = {
-        'proxy-ip': rs[1].split()[5],
-        'proxy-port': rs[1].split()[8],
-        'proxy-type': rs[3].split()[1:],
-        'username': '',
-        'password': ''
-    }
-    return settings
-
-
-def get_settings():
-    saved_settings = None
-
-    entries = read_file_contents_as_lines(dante_conf)
-    if not entries:
+def get_dante():
+    lines = read_file_contents_as_lines(dante_conf)
+    if not lines:
         return
 
-    parse_out(entries)
-    return saved_settings
+    proxyip = proxyport = proxytype = None
+    try:
+        for line in lines:
+            if line.startswith('from:'):
+                proxyip = line.split(' ')[5]
+                proxyport = line.split(' ')[8]
+            if line.startswith('proxyprotocol:'):
+                proxytype = line.split(' ')[1:]
+    except Exception:
+        return
 
-
-def set_settings(self, dict_settings):
-    bmarker = False
-    if dict_settings['proxy-type'] == 'http_v1.0':
-        new_settings = self.dante_format_http % (dict_settings)
+    if not (proxyip or proxyport or proxytype):
+        return
     else:
-        new_settings = self.dante_format_socks % (dict_settings)
-
-    # Read Dante's current settings
-    entries = read_file_contents_as_lines(self.filename)
-
-    # Override kano section with new settings
-    f = open(self.filename, 'w')
-    for c, e in enumerate(entries):
-        f.write(e)
-        if e.strip('\n') == self.marker:
-            # Kano marker found, stop here to override new settings
-            bmarker = True
-            break
-
-    if not bmarker:
-        f.write(self.marker + '\n')
-
-    f.write(new_settings)
-    f.close()
-    return True
+        return proxyip, proxyport, proxytype
 
 
+def update_ld_so_preload(enable):
+    lines = read_file_contents_as_lines(ld_so_preload_file)
 
-def is_enabled():
-    return libpreload.is_enabled()
+    # remove existing modules
+    if not enable:
+        new_file = [l for l in lines if l not in ld_so_preload_libs]
 
-
-def enable():
-    libpreload.proxify(True)
-
-
-def disable():
-    libpreload.proxify(False)
-
-
-def set_settings(proxyip, proxyport, proxytype, username=None, password=None):
-    settings = {
-        'proxy-ip': proxyip,
-        'proxy-port': proxyport,
-        'proxy-type': proxytype,   # one of : "socks_v4 socks_v5" or "http_v1.0"
-        'username': username,
-        'password': password
-    }
-    pxysettings.set_settings(settings)
-
-
-def get_settings():
-    return pxysettings.get_settings()
-
-
-def launch_chromium(widget=None, event=None):
-    user_name = get_user_unsudoed()
-    run_bg('su - ' + user_name + ' -c chromium')
-
-
-def network_info():
-    network = ''
-    command_ip = ''
-    command_network = '/sbin/iwconfig wlan0 | grep \'ESSID:\' | awk \'{print $4}\' | sed \'s/ESSID://g\' | sed \'s/\"//g\''
-    out, e, _ = run_cmd(command_network)
-    if e:
-        network = "Ethernet"
-        command_ip = '/sbin/ifconfig eth0 | grep inet | awk \'{print $2}\' | cut -d\':\' -f2'
+    # add module if not found
     else:
-        network = out
-        command_ip = '/sbin/ifconfig wlan0 | grep inet | awk \'{print $2}\' | cut -d\':\' -f2'
-    ip, _, _ = run_cmd(command_ip)
+        new_file = lines + [l for l in ld_so_preload_libs if l not in lines]
 
-    return [network.rstrip(), ip.rstrip()]
+    write_file_contents(ld_so_preload_file, new_file)
 
+
+def set_global(enable, ip, port, ptype, username, password):
+    set_chromium(enable, ip, port, ptype, username, password)
+    set_dante(enable, ip, port, ptype)
+
+
+def get_global():
+    return get_dante()
 
