5e29ed0d04de07ed9fcfacf5f8f8f6255e73c1d6
Tom Bettany <tom@kano.me>
2014-08-22 16:05:42 +0100

    Adds proxy for apt.

--- kano_settings/system/proxy.py
+++ kano_settings/system/proxy.py
@@ -8,9 +8,10 @@
 
 import os
 from kano.utils import run_cmd, get_all_home_folders, delete_file, \
-    write_file_contents, chown_path
+    write_file_contents, chown_path, read_file_contents_as_lines
 
 chromium_cfg = '/etc/chromium/default'
+apt_cfg = '/etc/apt/apt.conf.d/80proxy'
 
 
 def set_chromium(enable, ip, port):
@@ -58,6 +59,52 @@ def set_wget(enable, host, port, username=None, password=None):
 
 def set_all_proxies(enable, ip=None, port=None, username=None, password=None):
     set_chromium(enable, ip, port)
+
+
+def get_apt_proxy():
+    settings = {
+        'username': None,
+        'password': None,
+        'host': None,
+        'port': None
+    }
+
+    cfg = read_file_contents_as_lines(apt_cfg)
+
+    if not cfg:
+        return settings
+
+    for line in cfg:
+        if line.startswith('Acquire::'):
+            break
+
+    url = line.split('//')[-1].replace('/";', '')
+
+    if '@' in url:
+        (credentials, socket) = url.split('@')
+        (settings['username'], settings['password']) = credentials.split(':')
+    else:
+        socket = url
+
+    (settings['host'], settings['port']) = socket.split(':')
+
+    return settings
+
+
+def set_apt_proxy(enable, host, port, username=None, password=None):
+    apt_template = 'Acquire::http::proxy "http://{credentials}{host}:{port}/";'
+
+    if enable:
+        if username and password:
+            credentials = '{username}:{password}@'.format(username=username,
+                                                          password=password)
+        else:
+            credentials = ''
+        cfg = apt_template.format(credentials=credentials, host=host, port=port)
+    else:
+        cfg = ''
+
+    write_file_contents(apt_cfg, cfg)
 
 
 def get_all_proxies():
