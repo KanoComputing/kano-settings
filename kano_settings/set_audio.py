dee765c1e39294160de68bdbda50f6863851cb11
carolineclark <caroline@kano.me>
2014-08-26 13:26:39 +0100

    HDMI button was not correctly selected when HDMI audio was set

--- kano_settings/set_audio.py
+++ kano_settings/set_audio.py
@@ -83,8 +83,9 @@ class SetAudio(Template):
             self.win.go_to_home()
 
     def current_setting(self):
+        print is_HDMI()
         self.hdmi_button.set_active(is_HDMI())
-        self.analog_button.set_active(is_HDMI())
+        self.analog_button.set_active(not is_HDMI())
 
     def on_button_toggled(self, button):
         self.HDMI = button.get_active()
