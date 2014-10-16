62aaf0fd86d2bdf98d6f82e10bbb199d8cc79054
carolineclark <caroline@kano.me>
2014-10-16 11:44:55 +0100

    Modified set_notifications screen

--- kano_settings/set_notifications.py
+++ kano_settings/set_notifications.py
@@ -1,41 +1,64 @@
 #!/usr/bin/env python
-
+#
 # set_notifications.py
 #
 # Copyright (C) 2014 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
+# Controls the UI of the notification setting
 
+from gi.repository import Gdk
+from kano_settings.templates import CheckButtonTemplate
 from kano_settings.data import get_data
-from kano_settings.templates import Template
 
 
-class SetNotifications(Template):
-    selected_button = 0
-    initial_button = 0
-
+class SetNotifications(CheckButtonTemplate):
     data = get_data("SET_NOTIFICATIONS")
 
     def __init__(self, win):
 
-        title = self.data["LABEL_1"]
-        description = self.data["LABEL_2"]
-        kano_label = self.data["KANO_BUTTON"]
+        main_title = self.data["LABEL_1"]
+        main_description = self.data["LABEL_2"]
+        checkbox_desc_1 = self.data["DESCRIPTION_1"]
+        checkbox_desc_2 = self.data["DESCRIPTION_2"]
+        kano_button_label = self.data["KANO_BUTTON"]
 
-        Template.__init__(self, title, description, kano_label)
+        CheckButtonTemplate.__init__(self, main_title, main_description,
+                                     kano_button_label,
+                                     [[checkbox_desc_1, ""], [checkbox_desc_2, ""]])
 
         self.win = win
         self.win.set_main_widget(self)
-        self.top_bar.enable_prev()
 
         self.top_bar.enable_prev()
         self.top_bar.set_prev_callback(self.win.go_to_home)
 
-        self.kano_button.connect("button-release-event", self.win.go_to_home)
-        self.kano_button.connect("key-release-event", self.win.go_to_home)
+        self.disable_all_checkbutton = self.get_button(0)
+        self.disable_world_checkbutton = self.get_button(1)
 
-        # Refresh window
+        self.kano_button.connect("button-release-event", self.apply_changes)
+
         self.win.show_all()
 
-    def on_button_toggled(self, button):
-        pass
+    def configure_all_notifications(self):
+        if self.disable_all_checkbutton.get_active():
+            print "disable notifications"
+            pass
+        else:
+            print "enable notifications"
+            pass
+
+    def configure_world_notifications(self):
+        if self.disable_world_checkbutton.get_active():
+            print "disable notifications"
+            pass
+        else:
+            print "enable notifications"
+            pass
+
+    def apply_changes(self, widget, event):
+        if not hasattr(event, 'keyval') or event.keyval == Gdk.KEY_Return:
+            self.configure_all_notifications()
+            self.configure_world_notifications()
+            self.win.go_to_home()
+
