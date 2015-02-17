b81ee1174839bbd6a73136b12b7d05f1c634f46a
Alex Simon <alejandro@kano.me>
2015-02-17 10:27:20 +0000

    Added missing copyright

--- kano_settings/parental_config.py
+++ kano_settings/parental_config.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+
+# parental_config.py
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+
 from gi.repository import Gtk
 
 from kano import logging
