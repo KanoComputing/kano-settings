0c29035cc862fb900d59c34d00537549b45364d2
Alex Burr <alex.burr@kano.me>
2015-03-02 15:32:31 +0000

    Add support to boot_config.py for writing files other than /boot/config.txt This involved making the existing functions methods of an object containing the path, making the existing api a wrapper round a singleton which contains the /boot/config.txt path. The point of this is to allow saving the clock variables to a backup file in the same format.

--- kano_settings/boot_config.py
+++ kano_settings/boot_config.py
@@ -5,104 +5,135 @@
 # Copyright (C) 2014 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
-# Functions controlling reading and writing to /boot/confit.txt
+# Functions controlling reading and writing to /boot/config.txt
 #
 
 import re
+import os
 from kano.utils import read_file_contents_as_lines, is_number
 from kano.logging import logger
 
 boot_config_standard_path = "/boot/config.txt"
+boot_config_pi1_backup_path = "/boot/config_pi1_backup.txt"
+boot_config_pi2_backup_path = "/boot/config_pi2_backup.txt"
 
 
-# if the value argument is None, the option will be commented out
-def set_config_value(name, value=None,
-                     boot_config_path=boot_config_standard_path):
-    lines = read_file_contents_as_lines(boot_config_path)
-    if not lines:
-        return
+class BootConfig:
+    def __init__(self, path=boot_config_standard_path):
+        self.path = path
 
-    logger.info('writing value to /boot/config.txt {} {}'.format(name, value))
+    def exists(self):
+        return os.path.exists(self.path)
 
-    option_re = r'^\s*#?\s*' + str(name) + r'=(.*)'
+    def ensure_exists(self):
+        if not self.exists():
+            f = open(self.path, "w")
+            print >>f, "#"  # otherwise set_value thinks the file should not be written to
+            f.close()  # make file, even if empty
 
-    with open(boot_config_path, "w") as boot_config_file:
-        was_found = False
+    def set_value(self, name, value=None):
+        # if the value argument is None, the option will be commented out
+        lines = read_file_contents_as_lines(self.path)
+        if not lines:  # this is true if the file is empty, not sure that was intended.
+            return
 
-        for line in lines:
-            if re.match(option_re, line):
-                was_found = True
-                if value is not None:
-                    replace_str = str(name) + "=" + str(value)
+        logger.info('writing value to {} {} {}'.format(self.path, name, value))
+
+        option_re = r'^\s*#?\s*' + str(name) + r'=(.*)'
+
+        with open(self.path, "w") as boot_config_file:
+            was_found = False
+
+            for line in lines:
+                if re.match(option_re, line):
+                    was_found = True
+                    if value is not None:
+                        replace_str = str(name) + "=" + str(value)
+                    else:
+                        replace_str = r'#' + str(name) + r'=0'
+                    new_line = replace_str
                 else:
-                    replace_str = r'#' + str(name) + r'=0'
-                new_line = replace_str
-            else:
-                new_line = line
+                    new_line = line
 
-            boot_config_file.write(new_line + "\n")
+                boot_config_file.write(new_line + "\n")
 
-        if not was_found and value is not None:
-            boot_config_file.write(str(name) + "=" + str(value) + "\n")
+            if not was_found and value is not None:
+                boot_config_file.write(str(name) + "=" + str(value) + "\n")
 
+    def get_value(self, name):
+        lines = read_file_contents_as_lines(self.path)
+        if not lines:
+            return 0
 
-def get_config_value(name,
-                     boot_config_path=boot_config_standard_path):
+        for l in lines:
+            if l.startswith(name + '='):
+                value = l.split('=')[1]
+                if is_number(value):
+                    value = int(value)
+                return value
 
-    lines = read_file_contents_as_lines(boot_config_path)
-    if not lines:
         return 0
 
-    for l in lines:
-        if l.startswith(name + '='):
-            value = l.split('=')[1]
-            if is_number(value):
-                value = int(value)
-            return value
+    def set_comment(self, name, value):
+        lines = read_file_contents_as_lines(self.path)
+        if not lines:
+            return
 
-    return 0
+        logger.info('writing comment to {} {} {}'.format(self.path, name, value))
 
+        comment_str_full = '### {}: {}'.format(name, value)
+        comment_str_name = '### {}'.format(name)
 
-def set_config_comment(name, value,
-                     boot_config_path=boot_config_standard_path):
-    lines = read_file_contents_as_lines(boot_config_path)
-    if not lines:
-        return
+        with open(self.path, "w") as boot_config_file:
+            boot_config_file.write(comment_str_full + '\n')
 
-    logger.info('writing comment to /boot/config.txt {} {}'.format(name, value))
+            for line in lines:
+                if comment_str_name in line:
+                    continue
 
-    comment_str_full = '### {}: {}'.format(name, value)
-    comment_str_name = '### {}'.format(name)
+                boot_config_file.write(line + '\n')
 
-    with open(boot_config_path, "w") as boot_config_file:
-        boot_config_file.write(comment_str_full + '\n')
+    def get_comment(self, name, value):
+        lines = read_file_contents_as_lines(self.path)
+        if not lines:
+            return
 
-        for line in lines:
-            if comment_str_name in line:
-                continue
+        comment_str_full = '### {}: {}'.format(name, value)
+        return comment_str_full in lines
 
-            boot_config_file.write(line + '\n')
+    def has_comment(self, name):
+        lines = read_file_contents_as_lines(self.path)
+        if not lines:
+            return False
 
+        comment_start = '### {}:'.format(name)
+        for l in lines:
+            if l.startswith(comment_start):
+                return True
 
-def get_config_comment(name, value,
-                       boot_config_path=boot_config_standard_path):
-    lines = read_file_contents_as_lines(boot_config_path)
-    if not lines:
-        return
-
-    comment_str_full = '### {}: {}'.format(name, value)
-    return comment_str_full in lines
-
-
-def has_config_comment(name,
-                       boot_config_path=boot_config_standard_path):
-    lines = read_file_contents_as_lines(boot_config_path)
-    if not lines:
         return False
 
-    comment_start = '### {}:'.format(name)
-    for l in lines:
-        if l.startswith(comment_start):
-            return True
 
-    return False
+real_config = BootConfig()
+pi1_backup_config = BootConfig(boot_config_pi1_backup_path)
+pi2_backup_config = BootConfig(boot_config_pi2_backup_path)
+
+
+def set_config_value(name, value=None):
+    real_config.set_value(name, value)
+
+
+def get_config_value(name):
+    return real_config.get_value(name)
+
+
+def set_config_comment(name, value):
+    real_config.set_comment(name, value)
+
+
+def get_config_comment(name, value):
+    real_config.get_comment(name, value)
+
+
+def has_config_comment(name):
+    real_config.has_comment(name)
