535602e5991ace8303e1c05b847c6586242594aa
Alex Simon <alejandro@kano.me>
2015-04-14 15:15:32 +0100

    fixing license

--- kano_settings/boot_config.py
+++ kano_settings/boot_config.py
@@ -3,7 +3,7 @@
 # boot_config.py
 #
 # Copyright (C) 2014,2015 Kano Computing Ltd.
-# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU v2
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 # Functions controlling reading and writing to /boot/config.txt
 #
