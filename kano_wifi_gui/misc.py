b55e27f08805ae8f5e65c044bf6eda737e19f45a
carolineclark <caroline@kano.me>
2015-04-21 19:09:51 +0100

    Improved look of wifi gui

---
+++ kano_wifi_gui/misc.py
@@ -0,0 +1,24 @@
+
+from gi.repository import Gtk, GdkPixbuf
+import os
+
+from kano.paths import common_images_dir
+
+
+def tick_icon():
+    '''This should return a tick image.  We use this to show which
+    wifi network is already selected
+    '''
+
+    width = 24
+    height = 24
+
+    icons_path = os.path.join(common_images_dir, 'icons.png')
+
+    tick_pixbuf = GdkPixbuf.Pixbuf.new_from_file(icons_path)
+
+    tick_pixbuf = tick_pixbuf.new_subpixbuf(5 * 24, 0, width, height)
+    tick_image = Gtk.Image()
+    tick_image.set_from_pixbuf(tick_pixbuf)
+
+    return tick_image
