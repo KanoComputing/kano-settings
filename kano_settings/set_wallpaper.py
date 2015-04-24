dabb41e063939d5216b452afc6bee675ca54d312
Radek Pazdera <radek@kano.me>
2015-04-24 15:54:48 +0100

    Removing a bogus comment

--- kano_settings/set_wallpaper.py
+++ kano_settings/set_wallpaper.py
@@ -250,30 +250,3 @@ class FirstBootSetWallpaper(SetWallpaper):
     def apply_changes(self, button, event):
         SetWallpaper.apply_changes(self, button, event)
         Gtk.main_quit()
-#
-#        Gtk.Notebook.__init__(self)
-#
-#        background = Gtk.EventBox()
-#        background.get_style_context().add_class('set_appearance_window')
-#        background.add(self)
-#
-#        self.win = win
-#        self.win.set_main_widget(background)
-#        self.win.top_bar.enable_prev()
-#        self.win.change_prev_callback(self.win.go_to_home)
-#
-#        # Modify set_wallpaper so it doesn't add itself to the window
-#        wallpaper_widget = SetWallpaper(self.win)
-#        screensaver_widget = SetScreensaver(self.win)
-#        reset_widget = SetResetDesktop(self.win)
-#
-#        wallpaper_label = Gtk.Label('WALLPAPER')
-#        screensaver_label = Gtk.Label('SCREENSAVER')
-#        general_label = Gtk.Label('GENERAL')
-#
-#        # Add the screensaver and wallpaper tabs
-#        self.append_page(wallpaper_widget, wallpaper_label)
-#        self.append_page(screensaver_widget, screensaver_label)
-#        self.append_page(reset_widget, general_label)
-#
-#        self.win.show_all()
