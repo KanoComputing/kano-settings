0a9e98ff72b3cf2ac0c654aa18725ae2055a754d
Tom Bettany <tom@kano.me>
2015-06-16 15:46:00 +0100

    Settings Home Screen: Rework the way buttons for screens are managed
    Split out the screens and their home screen widgets to simplify adding
    and managing screens.

--- kano_settings/home_screen.py
+++ kano_settings/home_screen.py
@@ -2,33 +2,17 @@
 
 # home_screen.py
 #
-# Copyright (C) 2014 Kano Computing Ltd.
-# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+# Copyright (C) 2014-2015 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 
 from gi.repository import Gtk
-
-from kano_settings.set_keyboard import choose_keyboard_screen
-from kano_settings.set_mouse import SetMouse
-from kano_settings.set_notifications import SetNotifications
-from kano_settings.set_font import SetFont
-from kano_settings.set_audio import SetAudio
-from kano_settings.set_display import SetDisplay
-from kano_settings.set_wifi import SetWifi, SetProxy
-from kano_settings.no_internet_screen import NoInternet
-from kano_settings.set_overclock import SetOverclock
-from kano_settings.set_account import SetAccount
-from kano_settings.set_about import SetAbout
-from kano_settings.set_advanced import SetAdvanced
-from kano_settings.set_appearance import SetAppearance
-from kano_settings.set_wallpaper import FirstBootSetWallpaper
-
-import kano_settings.common as common
-from kano_settings.components.menu_button import Menu_button
-from kano_settings.config_file import get_setting
+import math
 
 from kano.gtk3.scrolled_window import ScrolledWindow
-from kano.utils import get_user_unsudoed
+
+from kano_settings.screens import SCREENS
+
 
 
 class UnknownScreenError(Exception):
@@ -36,165 +20,102 @@ class UnknownScreenError(Exception):
 
 
 class HomeScreen(Gtk.Box):
+    _DISPLAYED_SCREENS = SCREENS.get_screens_on_home()
 
-    names = ["Keyboard", "Mouse", "Audio", "Display", "Wifi", "Overclocking",
-             "Account", "Appearance", "Font", "Advanced", "About",
-             "Notifications"]
-    custom_info = ["Keyboard-country-human", "Mouse", "Audio", None, None,
-                   "Overclocking", None, "Wallpaper", "Font"]
+    def __init__(self, win, screen_number=None, screen_name=None):
+        self.win = win
 
-    def __init__(self, win, screen_number=None):
         # Check if we want to initialise another window first
+        if screen_name:
+            self.go_to_screen(screen_name)
+            return
+
         if screen_number is not None:
-            try:
-                self.state_to_widget(screen_number)(win)
-            except KeyError:
-                msg = "State {} not recognised".format(screen_number)
-                raise UnknownScreenError(msg)
+            self.go_to_screen_number(screen_number)
             return
 
         Gtk.Box.__init__(self)
 
-        self.win = win
         self.win.set_main_widget(self)
         self.win.top_bar.disable_prev()
         self.win.remove_prev_callback()
 
         self.width = 640
         self.height = 304
-        self.number_of_columns = 2
+        self._col_count = 2
 
-        self.generate_grid()
-        self.pack_start(self.scrolledwindow, True, True, 0)
+        scrolled_window = self._generate_grid()
+        self.pack_start(scrolled_window, True, True, 0)
 
         self.win.show_all()
 
-    def generate_grid(self):
-        buttons = []
-        self.labels = []
 
-        # names at top of file
-        for x in range(len(self.names)):
-            self.item = Menu_button(self.names[x], '')
-            self.labels.append(self.item.description)
-            # Update the state of the button, so we know which button has been
-            # clicked on.
-            self.item.button.state = x
-            self.item.button.connect("clicked", self.go_to_level)
-            buttons.append(self.item.button)
+    def _generate_grid(self):
+        row_count = int(math.ceil(
+            float(len(self._DISPLAYED_SCREENS)) / self._col_count)
+        )
 
-        # Fill the tabs with the current information
-        self.update_intro()
+        table = Gtk.Table(row_count, self._col_count, homogeneous=True,
+                          hexpand=True, vexpand=True)
+        table.props.margin = 0
 
-        # Create table
-        num_rows = len(self.names) / self.number_of_columns
-        if len(self.names) % self.number_of_columns != 0:  # Odd number of elements
-            num_rows += 1
-        self.table = Gtk.Table(num_rows, self.number_of_columns,
-                               True, hexpand=True, vexpand=True)
-        self.table.props.margin = 0
+        for idx, screen in enumerate(self._DISPLAYED_SCREENS):
+            row = idx / 2
+            col = idx % 2
 
-        # Attach to table
-        index = 0
-        row = 0
-        while index < len(self.names):
-            for j in range(0, self.number_of_columns):
-                if index < len(self.names):
-                    self.table.attach(buttons[index], j, j + 1, row, row + 1,
-                                      Gtk.AttachOptions.FILL | Gtk.AttachOptions.EXPAND,
-                                      Gtk.AttachOptions.FILL, 0, 0)
-                    if row % 2:
-                        buttons[index].get_style_context().add_class('appgrid_grey')
-                    else:
-                        buttons[index].get_style_context().add_class('appgrid_white')
-                    index += 1
-                else:
-                    break
-            row += 1
+            screen.create_menu_button(self._change_screen_cb)
 
-        # for scroll bar
-        self.scrolledwindow = ScrolledWindow(wide_scrollbar=True, vexpand=True,
-                                             hexpand=True)
-        self.scrolledwindow.get_style_context().add_class("app-grid")
-        self.scrolledwindow.add_with_viewport(self.table)
-
-    # This is to update the introduction text, so that if the settings are
-    # modified and then we go back to the intro screen, the latest information
-    # is shown
-    def update_intro(self):
-        for x in range(len(self.custom_info)):
-
-            if self.names[x] == 'Wifi':
-                text = ''
-                if common.has_internet:
-                    text = 'Connected'
-                else:
-                    text = 'Not connected'
-                self.labels[x].set_text(text)
-
-            elif self.names[x] == 'Account':
-                text = get_user_unsudoed()
-                self.labels[x].set_text(text)
-
-            elif self.names[x] == 'Display':
-                continue
-
+            if row % 2:
+                style_class = 'appgrid_grey'
             else:
-                self.labels[x].set_text(get_setting(self.custom_info[x]))
+                style_class = 'appgrid_white'
 
-    # When clicking next in the default intro screen - takes you to the last
-    # level you visited
-    def on_next(self, widget=None, arg2=None):
+            screen.menu_button.button.get_style_context().add_class(style_class)
 
-        self.state_to_widget(self.win.last_level_visited)(self.win)
-        self.win.last_level_visited = self.win.state
-        # Do not do win.show_all() as will stop the combotextbox in
-        # set_keyboard being hidden properly
+            screen.refresh_menu_button()
 
-    # On clicking a level button on default intro screen
-    def go_to_level(self, widget):
+            table.attach(screen.menu_button.button,
+                         col, col + 1, row, row + 1,
+                         Gtk.AttachOptions.FILL | Gtk.AttachOptions.EXPAND,
+                         Gtk.AttachOptions.FILL, 0, 0)
 
-        # Update current state
-        self.win.state = widget.state
-        # Record this level so we can go back to it
-        self.win.last_level_visited = self.win.state
 
-        # Call next state
+        scrolled_window = ScrolledWindow(wide_scrollbar=True, vexpand=True,
+                                         hexpand=True)
+        scrolled_window.get_style_context().add_class("app-grid")
+        scrolled_window.add_with_viewport(table)
+
+        return scrolled_window
+
+    def update_intro(self):
+        """
+        This is to update the introduction text, so that if the settings are
+        modified and then we go back to the intro screen, the latest information
+        is shown
+        """
+
+        for screen in self._DISPLAYED_SCREENS:
+            screen.refresh_menu_button()
+
+
+    def _change_screen_cb(self, screen, screen_name):
+        self.go_to_screen(screen_name)
+
+
+    def go_to_screen(self, screen_name):
+        self.win.state = screen_name
+        self.win.last_level_visited = screen_name
+
         self.win.clear_win()
-        self.state_to_widget(self.win.state)(self.win)
 
-    # On clicking a level button on default intro screen
-    def go_to_level_given_state(self, state):
+        try:
+            screen = SCREENS[screen_name]
+        except KeyError:
+            msg = "State {} not recognised".format(screen_name)
+            raise UnknownScreenError(msg)
 
-        # Remove element in the dynamic box
-        self.win.clear_win()
+        screen.screen_widget(self.win)
 
-        # Update current state
-        self.win.state = state
-        # Record this level so we can go back to it
-        self.win.last_level_visited = self.win.state
-
-        # Call next state
-        self.state_to_widget(self.win.state)(self.win)
-
-        # Refresh window
-        self.win.show_all()
-
-    def state_to_widget(self, x):
-        return {
-            0: choose_keyboard_screen,
-            1: SetMouse,
-            2: SetAudio,
-            3: SetDisplay,
-            4: SetWifi,
-            5: SetOverclock,
-            6: SetAccount,
-            7: SetAppearance,
-            8: SetFont,
-            9: SetAdvanced,
-            10: SetAbout,
-            11: SetNotifications,
-            12: NoInternet,
-            13: SetProxy,
-            14: FirstBootSetWallpaper
-        }[x]
+    def go_to_screen_number(self, screen_number):
+        screen = SCREENS.get_screen_from_number(screen_number)
+        screen.screen_widget(self.win)
