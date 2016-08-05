b9f3e5b3f383bd34a1eada197dd9e399121881e2
Tom Bettany <tom@kano.me>
2016-08-05 13:26:23 +0100

    Boot Config: Add function to construct filter

--- kano_settings/boot_config_filter.py
+++ kano_settings/boot_config_filter.py
@@ -1,2 +1,10 @@
 class Filter(object):
     ALL = 'all'
+    EDID_TEMPLATE='EDID={edid}'
+
+    @classmethod
+    def get_edid_filter(cls, edid):
+        if not edid:
+            return cls.ALL
+
+        return cls.EDID_TEMPLATE.format(edid=edid)
