d35b36b627a5dab811e68eb2f19dbf9665a075d7
Albert Casals <skarbat@gmail.com>
2017-03-17 14:45:25 +0000

    Adding a basic test for a given screen model

---
+++ tests/optimal_screen/test_optimal_screens.py
@@ -0,0 +1,57 @@
+#
+# test_optimal_screens.py
+#
+# Copyright (C) 2017 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# Tests for bin/kano-settings-onboot
+#
+# Using a collection of screen information dumps from several screen models,
+# the tests run the tool in test mode, and make sure the optimal resolutions
+# are found and selected.
+#
+# To add more use cases, run "sudo kano-settings-onboot --dump > myscreen.dump"
+# and add an additional test that checks for its optimal resolution / aspect ratio.
+#
+# TODO: Can these tests work inside the virtual QA sandbox?
+#  @unittest.skipUnless('-rpi' in sys.argv, 'Skipping because this is not a real RPI')
+#
+
+import os
+import unittest
+
+# Import kano-settings from this cloned repository
+import sys
+sys.path.insert(1, '../../')
+
+app='sudo ../../bin/kano-settings-onboot'
+
+class TestOptimalScreen(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        os.system('sudo kano-logs config -l debug -o debug')
+
+    @classmethod
+    def tearDownClass(cls):
+        os.system('sudo kano-logs config -l none -o none')
+        
+    def _test_dump(self, dumpfile):
+        command='{} --dry-run --verbose --force --test:{}'.format(app, dumpfile)
+        output=os.popen(command).read()
+        if verbose:
+            print output
+        return output
+
+    def test_adafruit_hdmi(self):
+        data = self._test_dump('screen-adafruit-hdmi.dump')
+        self.assertIn('Applying optimal mode', data)
+        self.assertIn('changes applied: True', data)
+        self.assertIn("u'resolution': u'1280x800', u'mode': 28, u'aspect': u'16:10'", data)
+
+
+
+if __name__ == '__main__':
+
+    verbose=os.getenv('VERBOSE')    
+    unittest.main()
