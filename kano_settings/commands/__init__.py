18c5d48b00a2048018012996fae0a7952717761b
Tom Bettany <tom@kano.me>
2018-01-17 14:11:09 +0000

    Migrate main out

---
+++ kano_settings/commands/__init__.py
@@ -0,0 +1,9 @@
+#
+# __init__.py
+#
+# Copyright (C) 2017 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPLv2
+#
+
+__author__ = 'Kano Computing Ltd.'
+__email__ = 'dev@kano.me'
